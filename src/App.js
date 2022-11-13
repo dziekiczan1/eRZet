8b0d83ccc2ac3efa8130827cd2e6b202441617bd
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-11-13 12:21:32 +0100

    feat: add html structure and nav logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import logo from "../src/logo.png";
+import circle from "../src/circle.png";
+import { motion } from "framer-motion";
+
 const App = () => {
   return (
-    <h1 className="bg-navy-700 text-pink-100 text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <header>
+        <nav className="flex flex-col items-center py-8">
+          <div className="relative w-1/2 mb-8">
+            <img src={logo} alt="eRZet" />
+            <img
+              src={circle}
+              alt="eRZet bg-circle"
+              width={250}
+              className="absolute top-0 left-[-100px] rotate-180 brightness-70"
+            />
+          </div>
+          <ul className="flex flex-row justify-between w-1/2 text-white">
+            <li>
+              <a href="#">One</a>
+            </li>
+            <li>
+              <a href="#">Two</a>
+            </li>
+            <li>
+              <a href="#">Three</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <h1>Main</h1>
+      </main>
+
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </>
   );
 };
 
