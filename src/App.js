35824fe5fe4e424d7da2033827dc7ff3325ac3b8
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-11-17 20:42:26 +0100

    feat: add drop card container

--- src/App.js
+++ src/App.js
@@ -14,6 +14,18 @@ const App = () => {
         <div className="h-[500px] bg-navy-500 w-full">
           <div className="h-full flex justify-center pt-20">
             <h1 className="text-white text-3xl font-bold">{t("welcome")}</h1>
+            <div className="container">
+              <div className="drop">
+                <div className="content">
+                  <h2>01</h2>
+                  <p>
+                    lorem ipsum dolor sit amet consecteu lorem ipsum dolor sit
+                    amet consecteu
+                  </p>
+                  <a href="#">Read More</a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
