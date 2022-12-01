8f7017fa0cda1750741b650869cb391de34822fc
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-12-01 19:56:48 +0100

    fix: change Navbar bg image

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,7 +12,13 @@ import azureCircle from "../../assets/azurecircle.png";
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-col items-center relative">
+    <nav className="flex flex-col items-center relative overflow-hidden navbar">
+      <img
+        src={goldRec}
+        alt=""
+        className="absolute h-[18rem] bottom-10 right-0 rotate-[270deg]"
+      />
+      <img src={azureRec} alt="" className="absolute h-[22rem] left-6 top-0" />
       <div className="flex justify-center w-full mb-8">
         <div className="flex justify-between w-3/4 text-xs text-white font-bold py-4">
           <div className="flex flex-row gap-4 py-2 px-6 items-center rounded-base shadow-baseSh">
