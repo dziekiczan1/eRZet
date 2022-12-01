d38053170640b52c6046538b3be6b9475d75b19a
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-12-01 16:23:44 +0100

    fix: change Toggle width and translate

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -25,22 +25,6 @@ const Navbar = () => {
       </div>
       <div className="flex justify-center relative w-1/2 mb-8 select-none">
         <img src={logo} alt="eRZet" className="z-50" />
-        {/* <img
-          src={circle}
-          alt="eRZet bg-circle"
-          width={210}
-          className="absolute -top-6 lg:left-[-22px] 2xl:left-[52px] rotate-[230deg] brightness-70"
-        /> */}
-        {/* <img
-          src={goldRec}
-          className="h-[697px] absolute -top-[8em] -right-[11em] rotate-90 z-10"
-          alt=""
-        /> */}
-        <img
-          src={azureCircle}
-          className="w-[180px] absolute rotate-[-90deg] opacity-25"
-          alt=""
-        />
       </div>
       <MainMenu menuItems={menuItems} />
     </nav>
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -16,7 +16,7 @@ const Toggle = () => {
           onClick={() => {
             setEnabled(!enabled);
           }}
-          className="w-12 h-4 bg-gray-200 rounded-full peer  peer-focus:ring-blue-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:transition-all peer-checked:bg-blue-600"
+          className="w-10 h-4 bg-gray-200 rounded-full peer  peer-focus:ring-blue-300  peer-checked:after:translate-x-6 peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:transition-all peer-checked:bg-blue-600"
         ></div>
       </label>
     </div>
