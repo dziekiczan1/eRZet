91854c7dbe545295d42566e53c7d9f433924065c
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-01-02 19:55:26 +0100

    feat: add svg to Pricing component

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,8 +5,10 @@ import PropTypes from "prop-types";
 function Button({ className, appearance, children }) {
   const styles = {
     base: "flex flex-row items-center justify-center gap-2 uppercase p-2 lg:p-4 text-white text-bold outline-offset-4 outline outline-2 outline-orange-100",
-    baseDark: "bg-navy-700 hover:bg-orange-100 hover:bg-opacity-20",
-    baseLight: "bg-slate-600 hover:bg-navy-700 hover:bg-opacity-50",
+    baseDark:
+      "bg-navy-700 hover:bg-orange-100 hover:bg-opacity-20 transition ease-in-out",
+    baseLight:
+      "bg-slate-600 hover:bg-navy-700 hover:bg-opacity-50 transition ease-in-out",
   };
 
   return (
