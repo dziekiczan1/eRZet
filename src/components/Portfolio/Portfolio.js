d46303954ed04f952af5e3da34c873abb6cce54a
Piotr Rzadkowolski <p.rzadkowolski@gmail.com>
2023-01-04 18:48:36 +0100

    fix: update style of slider arrows in Portfolio component

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -90,7 +90,7 @@ const Portfolio = () => {
                       >
                         <img
                           src={slide.imgSrc}
-                          className="w-auto h-full object-cover"
+                          className="w-full h-full object-cover"
                           alt={slide.title}
                           loading="lazy"
                         />
@@ -106,7 +106,7 @@ const Portfolio = () => {
                           >
                             <img
                               src={slide.companyLogo}
-                              className="w-full h-full object-contain"
+                              className="w-full h-full object-cover"
                               alt={slide.title}
                               loading="lazy"
                             />
@@ -154,35 +154,53 @@ const Portfolio = () => {
           className="absolute top-1/2 cursor-pointer left-2 lg:left-1/5"
           onClick={prevSlide}
         >
-          <svg
-            width="16"
-            height="26"
-            viewBox="0 0 8 13"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+          <div
+            className={
+              theme === "dark"
+                ? "bg-navy-700 rounded-full w-[60px] h-[60px] flex justify-center items-center shadow-md"
+                : "bg-light-100 rounded-full w-[60px] h-[60px] flex justify-center items-center shadow-md"
+            }
           >
-            <path
-              d="M7.20711 0.792891C7.59763 1.18342 7.59763 1.81658 7.20711 2.20711L2.91418 6.50001L7.20711 10.7929C7.59763 11.1834 7.59763 11.8166 7.20711 12.2071C6.81658 12.5976 6.1834 12.5976 5.79289 12.2071L0.792876 7.20711C0.402375 6.81661 0.402375 6.18341 0.792876 5.79291L5.79289 0.792891C6.1834 0.40237 6.81658 0.40237 7.20711 0.792891Z"
-              fill={theme === "dark" ? "#FFFFFF" : "#000000"}
-            />
-          </svg>
+            <svg
+              width="16"
+              height="26"
+              viewBox="0 0 8 13"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="relative right-[2px]"
+            >
+              <path
+                d="M7.20711 0.792891C7.59763 1.18342 7.59763 1.81658 7.20711 2.20711L2.91418 6.50001L7.20711 10.7929C7.59763 11.1834 7.59763 11.8166 7.20711 12.2071C6.81658 12.5976 6.1834 12.5976 5.79289 12.2071L0.792876 7.20711C0.402375 6.81661 0.402375 6.18341 0.792876 5.79291L5.79289 0.792891C6.1834 0.40237 6.81658 0.40237 7.20711 0.792891Z"
+                fill={theme === "dark" ? "#FFFFFF" : "#000000"}
+              />
+            </svg>
+          </div>
         </div>
         <div
           className="absolute top-1/2 cursor-pointer right-2 lg:right-1/5"
           onClick={nextSlide}
         >
-          <svg
-            width="16"
-            height="26"
-            viewBox="0 0 8 13"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+          <div
+            className={
+              theme === "dark"
+                ? "bg-navy-700 rounded-full w-[60px] h-[60px] flex justify-center items-center shadow-md"
+                : "bg-light-100 rounded-full w-[60px] h-[60px] flex justify-center items-center shadow-md"
+            }
           >
-            <path
-              d="M0.792891 0.792891C0.40237 1.18342 0.40237 1.81658 0.792891 2.20711L5.08582 6.50001L0.792891 10.7929C0.40237 11.1834 0.40237 11.8166 0.792891 12.2071C1.18342 12.5976 1.8166 12.5976 2.20711 12.2071L7.20712 7.20711C7.59763 6.81661 7.59763 6.18341 7.20712 5.79291L2.20711 0.792891C1.8166 0.40237 1.18342 0.40237 0.792891 0.792891Z"
-              fill={theme === "dark" ? "#FFFFFF" : "#000000"}
-            />
-          </svg>
+            <svg
+              width="16"
+              height="26"
+              viewBox="0 0 8 13"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="relative left-[2px]"
+            >
+              <path
+                d="M0.792891 0.792891C0.40237 1.18342 0.40237 1.81658 0.792891 2.20711L5.08582 6.50001L0.792891 10.7929C0.40237 11.1834 0.40237 11.8166 0.792891 12.2071C1.18342 12.5976 1.8166 12.5976 2.20711 12.2071L7.20712 7.20711C7.59763 6.81661 7.59763 6.18341 7.20712 5.79291L2.20711 0.792891C1.8166 0.40237 1.18342 0.40237 0.792891 0.792891Z"
+                fill={theme === "dark" ? "#edf2f8" : "#2d364c"}
+              />
+            </svg>
+          </div>
         </div>
       </motion.div>
     </motion.div>
