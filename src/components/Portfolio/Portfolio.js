ffb7c534725542d8a5272a91a97009d23fdfd96c
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-01-06 12:22:51 +0100

    fix: change svg height in Portfolio component on mobile

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -157,17 +157,15 @@ const Portfolio = () => {
           <div
             className={
               theme === "dark"
-                ? "bg-navy-700 rounded-full w-[60px] h-[60px] flex justify-center items-center shadow-md"
-                : "bg-light-100 rounded-full w-[60px] h-[60px] flex justify-center items-center shadow-md"
+                ? "bg-navy-700 rounded-full w-[30px] h-[30px] md:w-[60px] md:h-[60px] flex justify-center items-center shadow-md"
+                : "bg-light-100 rounded-full w-[30px] h-[30px] md:w-[60px] md:h-[60px] flex justify-center items-center shadow-md"
             }
           >
             <svg
-              width="16"
-              height="26"
               viewBox="0 0 8 13"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className="relative right-[2px]"
+              className="relative right-[2px] w-[12px] h-[18px] md:w-[16px] md:h-[26px]"
             >
               <path
                 d="M7.20711 0.792891C7.59763 1.18342 7.59763 1.81658 7.20711 2.20711L2.91418 6.50001L7.20711 10.7929C7.59763 11.1834 7.59763 11.8166 7.20711 12.2071C6.81658 12.5976 6.1834 12.5976 5.79289 12.2071L0.792876 7.20711C0.402375 6.81661 0.402375 6.18341 0.792876 5.79291L5.79289 0.792891C6.1834 0.40237 6.81658 0.40237 7.20711 0.792891Z"
@@ -183,17 +181,15 @@ const Portfolio = () => {
           <div
             className={
               theme === "dark"
-                ? "bg-navy-700 rounded-full w-[60px] h-[60px] flex justify-center items-center shadow-md"
-                : "bg-light-100 rounded-full w-[60px] h-[60px] flex justify-center items-center shadow-md"
+                ? "bg-navy-700 rounded-full w-[30px] h-[30px] md:w-[60px] md:h-[60px] flex justify-center items-center shadow-md"
+                : "bg-light-100 rounded-full w-[30px] h-[30px] md:w-[60px] md:h-[60px] flex justify-center items-center shadow-md"
             }
           >
             <svg
-              width="16"
-              height="26"
               viewBox="0 0 8 13"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className="relative left-[2px]"
+              className="relative left-[2px] w-[12px] h-[18px] md:w-[16px] md:h-[26px]"
             >
               <path
                 d="M0.792891 0.792891C0.40237 1.18342 0.40237 1.81658 0.792891 2.20711L5.08582 6.50001L0.792891 10.7929C0.40237 11.1834 0.40237 11.8166 0.792891 12.2071C1.18342 12.5976 1.8166 12.5976 2.20711 12.2071L7.20712 7.20711C7.59763 6.81661 7.59763 6.18341 7.20712 5.79291L2.20711 0.792891C1.8166 0.40237 1.18342 0.40237 0.792891 0.792891Z"
